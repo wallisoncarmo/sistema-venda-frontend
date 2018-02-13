f159dfd1e70ff2cf86dfe2c30943551328a32dda
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-02-12 22:54:13 -0200

    [ADD] View da lista de categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 
 @IonicPage()
@@ -10,6 +12,8 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  items: CategoriaDTO[];
+  bucketBaseUrl: string=API_CONFIG.bucketBaseUrl;
 
   constructor(
     public navCtrl: NavController,
@@ -20,7 +24,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       }
       , error => {
         console.log(error);
