0b12349dacb0e9c954d7d3c31936e24481382434
Wallison do Carmo <wallisoncarmo01@gmail.com>
2018-02-13 19:02:00 -0200

    [ADD]Adicao da imagem ao perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the ProfilePage page.
@@ -17,12 +20,13 @@ import { StorageService } from '../../services/storage.service';
 export class ProfilePage {
 
 
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public storage: StorageService
+    public storage: StorageService,
+    public clienteService: ClienteService
   ) {
   }
 
@@ -30,8 +34,22 @@ export class ProfilePage {
     let localUser = this.storage.getLocalUser();
 
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.clienteService.findByEmail(localUser.email).subscribe(response => {
+        this.cliente = response;
+
+        this.getImageIfExists();
+
+      },
+        error => { });
     }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+      error => { });
   }
 
 }
