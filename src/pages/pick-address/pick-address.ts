784f89c5cb35d1b9d41f4760409a176fe9a03fb6
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-15 17:39:32 -0300

    [ADD] Armazenamento dos pedidos

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/domain/cart.service';
+import { PedidoDTO } from './../../models/pedido.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
@@ -13,11 +15,14 @@ export class PickAddressPage {
 
   items: EnderecoDTO[];
 
+  pedido: PedidoDTO;
+
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -26,16 +31,32 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
-        },
-        error => {
-          if (error.status == 403) {
-            this.navCtrl.setRoot('HomePage');
+
+
+          let cart = this.cartService.getCart();
+
+            this.pedido = {
+              cliente: { id: response['id'] },
+              enderecoDeEntrega: null,
+              pagamento: null,
+              itens:cart.items.map(x=>{ return {quantidade:x.quantidade, produto:{id:x.produto.id}} })
           }
-        });
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
     }
     else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+
+  nextPage(item:EnderecoDTO){
+    this.pedido.enderecoDeEntrega ={id: item.id};
+    console.log(this.pedido);
   }
 
 }
