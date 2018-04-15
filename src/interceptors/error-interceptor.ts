0c24cb3f3bfcf0a9bd53ba074953fc60f7462ac8
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-15 11:57:11 -0300

    [ADD] Tratamento padrao para outros erros

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
+import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+
+    
+    constructor (
+        public storage:StorageService,
+        public alertController: AlertController
+    ){}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       
@@ -20,8 +28,51 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
 
+            switch(errorObj.status){
+                case 401:
+                this.handle401();
+                break;
+
+                case 403:
+                this.handle403();
+                break;
+
+                default:
+                this.handleDefaultError(errorObj);
+            }
+
             return Observable.throw(errorObj);
         }) as any;
+    }
+
+    handle401(){
+        let alert = this.alertController.create({
+            title:'Falha de Autenticação',
+            message:'E-mail ou senha Incorretos',
+            enableBackdropDismiss:false,
+            buttons:[
+                {text:'Ok'}
+            ]
+        });
+
+        alert.present();
+    }
+
+    handle403(){
+        this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(error) {
+        let alert = this.alertController.create({
+            title:'Erro'+error.status+': '+error.error,
+            message:error.message,
+            enableBackdropDismiss:false,
+            buttons:[
+                {text:'Ok'}
+            ]
+        });
+
+        alert.present();
     }
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -40,7 +40,13 @@ export class ProfilePage {
         this.getImageIfExists();
 
       },
-        error => { });
+        error => {
+          if( error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }else{
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
