592137741929f779f9b2e93a5ffabdfaf8393800
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-16 01:21:14 -0300

    [FIX] Correcao na atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,13 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -20,19 +14,20 @@ import { CameraOptions, Camera } from '@ionic-native/camera';
 })
 export class ProfilePage {
 
-
   cliente: ClienteDTO;
-
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(
-    public navCtrl: NavController,
+    public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera
-  ) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -41,32 +36,46 @@ export class ProfilePage {
 
   loadData() {
     let localUser = this.storage.getLocalUser();
-
     if (localUser && localUser.email) {
-      this.clienteService.findByEmail(localUser.email).subscribe(response => {
-        this.cliente = response as ClienteDTO;
-
-        this.getImageIfExists();
-
-      },
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
+          this.cliente = response as ClienteDTO;
+          this.getImageIfExists();
+        },
         error => {
           if (error.status == 403) {
             this.navCtrl.setRoot('HomePage');
           }
         });
-    } else {
-      this.navCtrl.setRoot('HomePage');
     }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }    
   }
 
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-      },
-        error => { });
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
+    },
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
   }
 
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
 
   getCameraPicture() {
 
@@ -78,11 +87,12 @@ export class ProfilePage {
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
-
+    
     this.camera.getPicture(options).then((imageData) => {
-      this.picture = 'data:image/png;base64,' + imageData;
-      this.cameraOn = false;
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
@@ -97,11 +107,12 @@ export class ProfilePage {
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
-
+    
     this.camera.getPicture(options).then((imageData) => {
-      this.picture = 'data:image/png;base64,' + imageData;
-      this.cameraOn = false;
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
     }, (err) => {
+      this.cameraOn = false;
     });
   }
 
@@ -109,10 +120,10 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       },
-        error => {
-        });
+      error => {
+      });
   }
 
   cancel() {
