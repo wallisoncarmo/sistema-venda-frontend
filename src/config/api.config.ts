540b1694fbba8a71b658ea51b512e4b566275c76
Wallison do Carmo Costa <wallisoncarmo01@gmail.com>
2018-04-16 00:22:58 -0300

    [ADD] Preparando o ambiente de producao

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG={
-    baseUrl: "http://192.168.25.11:8080",
+   // baseUrl: "http://192.168.25.11:8080",
+    baseUrl: "https://sistema-venda.herokuapp.com/",
     bucketBaseUrl:"https://s3-sa-east-1.amazonaws.com/sistema-venda",
 }
